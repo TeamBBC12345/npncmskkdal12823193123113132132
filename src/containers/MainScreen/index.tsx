26079bf18561277928a766df0dff923eb9c6adb9
Error4046716 <91386472+Error4046716@users.noreply.github.com>
2021-10-04 16:41:30 +0800

    Update index.tsx

--- src/containers/MainScreen/index.tsx
+++ src/containers/MainScreen/index.tsx
@@ -36,7 +36,7 @@ const tabsArr = ({ t }: { t: TFunction }) => [
     key: tabs.TRAVEL_RECORD,
     label: t("travel_record.name"),
     component: <TravelRecord />,
-    icon: ,
+    icon: <PlaceIcon />,
   },
   {
     key: tabs.BOOKMARK,
