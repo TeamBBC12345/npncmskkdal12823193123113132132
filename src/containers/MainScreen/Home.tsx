b074ec12c7849cda25f2db77b16e9c0bebf9c6eb
Error4046716 <91386472+Error4046716@users.noreply.github.com>
2021-10-05 00:19:27 +0800

    Update Home.tsx

--- src/containers/MainScreen/Home.tsx
+++ src/containers/MainScreen/Home.tsx
@@ -247,6 +247,7 @@ const Firstcss001 = styled.div`
     font-size: 28px;
     color: white;
     font-weight: bold;
+    line-height: 1;
 `;
 
 const Card1 = styled.div`
@@ -289,6 +290,7 @@ const StyledPlace = styled(Place)`
   text-shadow: 0px 1px 2px rgba(0, 0, 0, 0);
   color: white;
   padding-bottom: 0px;
+  line-height: 15px;
 `;
 
 const SliderWrapper = styled.div`
