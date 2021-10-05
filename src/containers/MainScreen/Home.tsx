531d9d24a314ca4269981addc9848543667b9541
Error4046716 <91386472+Error4046716@users.noreply.github.com>
2021-10-06 00:52:51 +0800

    Update Home.tsx

--- src/containers/MainScreen/Home.tsx
+++ src/containers/MainScreen/Home.tsx
@@ -192,7 +192,11 @@ export const Home = () => {
                   padding: "3px 0px 3px 0px",
                 }}>
               <Link to="/qrReader">
-                <Button color="primary" style={{
+                <Button 
+                    disabled={isEmpty(trim(license))}
+                    onClick={handleTaxiSubmit}
+                    color="primary" 
+                    style={{
                     backgroundColor: "white",
                     padding: "1px 1px 1px 15px",
                     fontSize: "18px",
