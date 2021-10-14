2be9d8839103a3184be95ee91bddbac8dbb7f93d
Team68901928374123 <danielc00900@gmail.com>
2021-10-14 20:00:33 +0800

    Update TravelRecord.tsx

--- src/containers/MainScreen/TravelRecord.tsx
+++ src/containers/MainScreen/TravelRecord.tsx
@@ -18,7 +18,7 @@ import LocalTaxiIcon from "@material-ui/icons/LocalTaxi";
 import StoreIcon from "@material-ui/icons/Store";
 import dayjs from "dayjs";
 import { isEmpty } from "ramda";
-import React from "react";
+// import React from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 
